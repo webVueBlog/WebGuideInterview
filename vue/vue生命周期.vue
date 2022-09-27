b3ea9c415b27b0690657b4f5a902bf244345abb0
webVueBlog <2979986590@qq.com>
2022-09-28 01:18:00 +0800

    docs: fix message

---
+++ vue/vue生命周期.vue
@@ -0,0 +1,32 @@
+new Vue({
+	el: '#demo',
+	data: {
+		classA: true,
+		list: [1,2,3,4,5,6],
+		message: 'Hello Vue',
+	},
+	beforeCreate() {
+		
+	},
+	created() {
+		
+	},
+	beforeMount() {
+		
+	},
+	mounted() {
+		
+	},
+	beforeUpdate() {
+		
+	},
+	update() {
+		
+	},
+	beforeDestroy() {
+		
+	},
+	destroyed() {
+		
+	}
+})
