e9cda39edf9472a51198d8c1346a26ddec147f55
webVueBlog <2979986590@qq.com>
2022-09-29 09:50:28 +0800

    docs

--- vue/directive.vue
+++ vue/directive.vue
@@ -2,6 +2,14 @@
 </template>
 
 <script>
+	// 注册一个全局自定义指令
+	Vue.directive('userdefined', {
+		// 当被绑定的元素插入到 DOM 中时。。。
+		inserted: function (el) {
+			// 聚焦元素
+			
+		}
+	})
 </script>
 
 <style>
--- vue/provide组件通信.vue
+++ vue/provide组件通信.vue
@@ -1,10 +1,59 @@
 <template>
 	<div>
 		{{ message }}
+		<div v-for="(item, index) in list"></div>
 	</div>
 </template>
 
 <script>
+	Vue.component('button-counter', {
+		props: ['id'],
+		functional: true,
+		inject: ['foo'],
+		data: function() {
+			return {
+				count: 0
+			}
+		},
+		mounted: () => {
+			// 挂载元素，获取到DOM节点
+		},
+		template: '<button>按钮</button>'
+	})
+	
+	new Vue({el: '#components-demo'})
+	
+	var app = new Vue({
+		provide: {
+			foo: 'bar'
+		},
+		beforeCreate() {
+			
+		},
+		created() {
+			
+		},
+		beforeMount() {
+			
+		},
+		mounted() {
+			setTimeout(() => {
+				console.log('xxx')
+			})
+		},
+		beforeUpdate() {
+			
+		},
+		updated() {
+			
+		},
+		beforeDestroy() {
+			
+		},
+		destroyed() {
+			
+		}
+	})
 </script>
 
 <style>
