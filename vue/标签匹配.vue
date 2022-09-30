76bde83d063203704dc020be4dbc576931f4b7fb
webVueBlog <2979986590@qq.com>
2022-09-30 23:02:59 +0800

    docs

---
+++ vue/标签匹配.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<input type="text" v-info v-data/>
+	</div>
+</template>
+
+<script>
+	// 注册一个全局自定义指令 v-focus
+	Vue.directive('info', {
+		// 当被绑定的元素插入到DOM中时
+	})
+</script>
+
+<style>
+</style>
