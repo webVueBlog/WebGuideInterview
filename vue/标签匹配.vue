fc718ca7e8565aa8584da5520bb2db037cc0911c
webVueBlog <2979986590@qq.com>
2022-10-01 11:33:13 +0800

    feat： vue

--- vue/标签匹配.vue
+++ vue/标签匹配.vue
@@ -7,7 +7,37 @@
 <script>
 	// 注册一个全局自定义指令 v-focus
 	Vue.directive('info', {
-		// 当被绑定的元素插入到DOM中时
+		// 当被绑定的元素插入到DOM中时...
+		inserted: function(el) {
+			el.addEventListener('change', function() {
+				console.log(this.value)
+			}, false)
+		}
+	})
+	
+	// 注册一个全局自定义指令 v-focus
+	Vue.directive('data', {
+		// 当被绑定的元素插入到DOM中时...
+		inserted: function(el) {
+			el.addEventListener('change', function() {
+				console.log(this.value)
+			}, false)
+		}
+	})
+	
+	new Vue({
+		el: '#app',
+		data: {
+			message: 'Hello'
+		},
+		directives: {
+			focus: {
+				// 指令的定义
+				inserted: function(el) {
+					el.focus()
+				}
+			}
+		}
 	})
 </script>
 
---
+++ vue/高阶组件.vue
@@ -0,0 +1,59 @@
+<template>
+	<div id="app">
+		<input v-model="query"/>
+		<my-transition :query="query" :list="list">
+			<li
+				v-for="(item, index) in computedList"
+				:key="item.msg"
+				:data-index="index"
+				{{item.msg}}
+			>
+			</li>
+		</my-transition>
+	</div>
+</template>
+
+<script>
+	Vue.component('my-transition', {
+		functional: true,
+		render: function(h, ctx) {
+			var data = {
+				props: {
+					tag: 'ul',
+					css: false
+				},
+				on: {
+					beforeEnter: function(el) {
+						el.style.opacity = 0
+						el.style.height = 0
+					},
+					enter: function(el, done) {
+						var delay = el.dataset.index * 150
+						setTimeout(function() {
+							Volocity(el, 
+							{opacity: 1, height: '1.6em'},
+							{complete: done})
+						}, delay)
+					},
+					leave: function(el, done) {
+						var delay = el.dataset.index * 150
+						setTimeout(function() {
+							Velocity(el, 
+							{opacity: 0, height: 0},
+							{complete: done})
+						}, delay)
+					}
+				}
+			}
+			return h('transition-group', data, ctx.children)
+		},
+		props: ['query', 'list']
+	})
+	
+	var app = new Vue({
+		
+	})
+</script>
+
+<style>
+</style>
